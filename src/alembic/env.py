fdb1a3bb0e074d69c6242b5e3c03cc0bbfbed0a9
andreas <podovinov2003andrey@gmail.com>
2023-02-20 22:08:13 +0600

    add alembic

---
+++ src/alembic/env.py
@@ -0,0 +1,42 @@
+import sys, os
+from pathlib import Path
+
+if (dir := str(Path(os.getcwd()).parent)) not in sys.path:
+    sys.path.append(dir)
+
+from alembic import context
+from src.db.db import connection_string, engine
+from src.models import base, users, bots, roles, relations
+
+
+target_metadata = base.Base.metadata
+
+
+def run_migrations_offline() -> None:
+    context.configure(
+        url=connection_string,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    connectable = engine
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
