17f1de657b4ebd21fd849876d070c5cf39ae6f42
andreas <podovinov2003andrey@gmail.com>
2023-03-11 16:45:08 +0600

    fix router

--- src/api/admin_chat.py
+++ src/api/admin_chat.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from src.services.auth import get_current_user
 from src.services.admin_chat import AdminChatService
 from src.models.schemas.admin_chat.admin_chat_request import AdminChatRequest
 from src.models.schemas.intents.intents_response import IntentsResponse
@@ -7,6 +8,7 @@ from src.models.schemas.intents.intents_response import IntentsResponse
 router = APIRouter(
     prefix='/admin_chat',
     tags=['admin_chat'],
+    dependencies=[Depends(get_current_user)],
 )
 
 
