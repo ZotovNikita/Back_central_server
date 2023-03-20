e34301424fd72f8bf6781feeb3f89b0d73ea0e3f
andreas <podovinov2003andrey@gmail.com>
2023-03-20 21:56:37 +0600

    add functions to intents

--- src/api/intents.py
+++ src/api/intents.py
@@ -1,8 +1,10 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, status, Depends
 from pydantic import UUID4
+from src.api.utils.admin_checker import IS_ADMIN
 from src.services.auth import get_current_user
 from src.services.intents import IntentsService
+from src.models.schemas.intents.intents_request import IntentsRequest, IntentsRequestAdmin
 from src.models.schemas.intents.intents_response import IntentsResponse
 
 
@@ -11,6 +13,31 @@ router = APIRouter(
     tags=['intents'],
     dependencies=[Depends(get_current_user)],
 )
+
+
+@router.get('/{id}', response_model=IntentsResponse, name='Получить интент по id', dependencies=[Depends(IS_ADMIN)])
+def get(id: int, service: IntentsService = Depends()):
+    return service.get(id)
+
+
+@router.get('/', response_model=List[IntentsResponse], name='Получить все интенты', dependencies=[Depends(IS_ADMIN)])
+def all(service: IntentsService = Depends()):
+    return service.all()
+
+
+@router.post('/', response_model=IntentsResponse, status_code=status.HTTP_201_CREATED, name='Добавить интент (warning)', dependencies=[Depends(IS_ADMIN)])
+def add(request: IntentsRequestAdmin, service: IntentsService = Depends(), user: dict = Depends(get_current_user)):
+    return service.add(request, user)
+
+
+@router.post('/{id}', response_model=IntentsResponse, name='Изменить интент (warning)', dependencies=[Depends(IS_ADMIN)])
+def update(id: int, request: IntentsRequestAdmin, service: IntentsService = Depends()):
+    return service.update(id, request)
+
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT, name='Удалить интент (warning)', dependencies=[Depends(IS_ADMIN)])
+def delete(id: int, service: IntentsService = Depends()):
+    return service.delete(id)
 
 
 @router.get('/{bot_guid}', response_model=List[IntentsResponse], name='Получить все интенты бота')
--- src/services/intents.py
+++ src/services/intents.py
@@ -2,12 +2,89 @@ from typing import List
 from fastapi import HTTPException, status, Depends
 from sqlalchemy.orm import Session
 from src.db.db import get_session
+from src.services.users import create_by
 from src.models.intents import Intents
+from src.models.schemas.intents.intents_request import IntentsRequest, IntentsRequestAdmin
 
 
 class IntentsService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
+    
+    def get(self, id: int) -> Intents:
+        intent = (
+            self.session
+            .query(Intents)
+            .filter(Intents.id == id)
+            .one_or_none()
+        )
+        
+        if not intent:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Интент не найден')
+        
+        return intent
+
+    def all(self) -> List[Intents]:
+        intents = (
+            self.session
+            .query(Intents)
+            .all()
+        )
+        
+        if not intents:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        
+        return intents
+    
+    def get_by_bot_guid_and_name(self, bot_guid: str, name: str) -> Intents:
+        """
+        Интенты имеют уникальные name для каждого бота, т.е. unique(name, bot_guid).
+        """
+        intent = (
+            self.session
+            .query(Intents)
+            .filter_by(bot_guid=bot_guid, name=name)
+            .one_or_none()
+        )
+        
+        if not intent:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Интент не найден')
+        
+        return intent
+
+    def add(self, request: IntentsRequestAdmin, user: dict) -> Intents:
+        is_exist = (
+            self.session
+            .query(Intents)
+            .filter_by(bot_guid=request.bot_guid, name=request.name)
+            .count()
+        )
+        if is_exist:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT)
+
+        intent = create_by(Intents(), request, user)
+
+        self.session.add(intent)
+        self.session.commit()
+        return intent
+
+    def update(self, id: int, request: IntentsRequestAdmin) -> Intents:
+        intent = self.get(id)
+        same_intent = self.get_by_bot_guid_and_name(request.bot_guid, request.name)
+        
+        if same_intent and id != same_intent.id:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT)
+
+        for field, value in request:
+            setattr(intent, field, value)
+
+        self.session.commit()
+        return intent
+
+    def delete(self, id: int) -> None:
+        intent = self.get(id)
+        self.session.delete(intent)
+        self.session.commit()
     
     def get_by_guid_and_rank(self, bot_guid: str, intent_rank: int) -> Intents:
         intent = (
